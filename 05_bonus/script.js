9bd52fcbcd708c2fa6736e1ce81c29464e7680c5
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-16 12:30:24 +0100

    ADD: function

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,20 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+// Function
+function greetingUser(string) {
+    let whatTimeIsIt = new Date().getHours();
+    let greeting;
 
+    (whatTimeIsIt <= 13) ? greeting = `Buongiorno ${string}` :
+    (whatTimeIsIt <= 17) ? greeting = `Buon pomeriggio ${string}` :
+    greeting = `Buonasera ${string}`
+
+    return greeting
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetingUser(name))
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
