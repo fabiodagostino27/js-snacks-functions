a95b00958e52c0a237959e1fde4990e9992c5603
fabiodagostino27 <fabiodago27@gmail.com>
2025-01-16 12:15:40 +0100

    ADD: function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+// Function
+function vowels(string) {
+    let arrVowels = [];
 
+    for (i = 0; i < string.length; i++) {
+        if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+            arrVowels.push(string.charAt(i))
+        } 
+    }
+
+    return arrVowels
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vowels(word).length, vowels(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
